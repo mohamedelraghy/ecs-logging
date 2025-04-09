39c81879a14792508f78ed1a4d9d54138abb91ad
Mohamed EL-Raghy <mohamed.elraghy@hamzahllc.com>
2025-04-09 16:07:17 +0200

    create elasticsearch client

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { initializeElasticsearchConnection } from "./config/db/elasticsearch";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Initialize the Elasticsearch connection at app startup
+initializeElasticsearchConnection();
 
 app.get("/", (_req, res) => {
   res.json({ message: "Hello, Express + TypeScript!" });
