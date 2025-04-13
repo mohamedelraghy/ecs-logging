280bcb67ac3a870383a89adc57e8d80daa41c873
Mohamed EL-Raghy <mohamed.elraghy@hamzahllc.com>
2025-04-13 16:46:20 +0200

    add logger to health check

--- src/features/healthCheck/routes/index.ts
+++ src/features/healthCheck/routes/index.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 
 import elasticClient from "../../../config/db/elasticsearch";
+import logger from "../../../utils/logger";
 
 const router = express.Router();
 
@@ -15,17 +16,20 @@ router.get("/", async (_req: Request, res: Response, _next: NextFunction) => {
     const isHealthy = ["green", "yellow"].includes(health.status); // yellow is acceptable in single-node
 
     if (isHealthy) {
+      logger.info(health);
       res.status(200).json({
         status: "ok",
         elasticsearch: health,
       });
     } else {
+      logger.error(health);
       res.status(503).json({
         status: "unhealthy",
         elasticsearch: health,
       });
     }
   } catch (error) {
+    logger.error(error);
     res.status(500).json({
       status: "error",
       message: "❌ Failed to connect to Elasticsearch",
