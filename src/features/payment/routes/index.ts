d9191836cff30801fbb0d06f2e28d0be1a0bc539
Mohamed EL-Raghy <elraghy8@gmail.com>
2025-04-21 13:53:28 +0200

    create mock api to log

---
+++ src/features/payment/routes/index.ts
@@ -0,0 +1,11 @@
+import express, { NextFunction, Request, Response } from "express";
+import logger from "../../../utils/logger";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  logger.info("Payment route hit", { req, res });
+  res.status(200).json({ message: "Payment route hit" });
+});
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import healthCheckRouter from "../features/healthCheck/routes";
+import paymentRouter from "../features/payment/routes";
 import statsRouter from "../features/stats/routes";
 
 const router = express.Router();
 
 router.use("/health", healthCheckRouter);
 router.use("/stats", statsRouter);
+router.use("/payment", paymentRouter);
 
 export default router;
